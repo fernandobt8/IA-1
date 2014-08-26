d89fb831e6d95440542137e4f510d9351513eef8
Felipe Nedel <felipenedel@gmail.com>
2014-08-25 22:19:56 -0300

    [up] Método dos vizinhos implementado;

--- src/utilidade/UtilityTest.java
+++ src/utilidade/UtilityTest.java
@@ -9,7 +9,7 @@ public class UtilityTest {
 
 	public static void main(String[] args) {
 		initBoard();
-		calculateUtility();
+		System.out.println(UtilidadeVizinhos.calculateUtility(board));
 	}
 
 	private static void initBoard() {
@@ -29,45 +29,6 @@ public class UtilityTest {
 		}
 
 		board.printBoard();
-	}
-
-	private static void calculateUtility() {
-		System.out.println();
-
-		for (int x = 0; x < board.HEIGHT; x++) {
-			for (int y = 0; y < board.WIDTH; y++) {
-				int utilidade = 0;
-				Token token = board.getCasa(x, y).getTipoCasa();
-
-				if (token != Token.BLANK) {
-					for (int i = 0; i < 3; i++) {
-						for (int j = 0; j < 3; j++) {
-
-							int casaX = x - 1 + i;
-							int casaY = y - 1 + j;
-
-							if (casaX >= 0 && casaY >= 0 && casaX < board.HEIGHT && casaY < board.WIDTH) {
-								utilidade += calcularUtilidade(token, board.getCasa(casaX, casaY).getTipoCasa());
-							}
-						}
-					}
-
-					// utilidade = (int) Math.pow(10, utilidade);
-					board.getCasa(x, y).setUtilidade(utilidade);
-				}
-				System.out.print(utilidade + "\t");
-			}
-			System.out.println();
-		}
-	}
-
-	private static int calcularUtilidade(Token tokenCentral, Token tokenAdjacente) {
-		if (tokenAdjacente == Token.BLANK) {
-			return 0;
-		} else if (tokenCentral == tokenAdjacente) {
-			return 1;
-		}
-		return -1;
 	}
 
 }
