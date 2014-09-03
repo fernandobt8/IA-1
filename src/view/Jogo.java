2ba4233fc5fc395eb9150b00c078c5a733e76702
Felipe Nedel <felipenedel@gmail.com>
2014-09-03 19:07:29 -0300

    [up] Implementando quem inicia jogando;

--- src/view/Jogo.java
+++ src/view/Jogo.java
@@ -10,13 +10,18 @@ public class Jogo {
 	private Arvore computer;
 	private UserViewImpl userView;
 
-	public Jogo(int dificuldade) {
+	public Jogo(int dificuldade, int primeiroJogador) {
 		this.tabuleiro = new Tabuleiro();
 		this.computer = new Arvore(dificuldade);
 		this.userView = new UserViewImpl(this);
 		this.userView.pack();
 		this.userView.setLocationRelativeTo(null);
 		this.userView.setVisible(true);
+
+		if (primeiroJogador == 1) {
+			this.tabuleiro = this.computer.getJogada(this.tabuleiro);
+			this.userView.updateTableModel(this.tabuleiro);
+		}
 	}
 
 	public void jogar(int row, int col) {
